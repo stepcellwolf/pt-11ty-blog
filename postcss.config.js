b3d07c0c13282556d8a8d7cd967a3d08966fd8dc
Predrag Tasevski <p.tasevski@unicis.tech>
2026-01-26 01:01:01 +0100

    favicon change

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: {
     'postcss-import': {},
     '@tailwindcss/postcss': {},
+    'autoprefixer': {},
     ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
   },
 }
